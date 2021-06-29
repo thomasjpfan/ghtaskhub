4d46c2919ac1c636747146b9cb53ca8610649d36
Thomas J. Fan <thomasjpfan@gmail.com>
2021-06-29 14:25:12 -0400

    ENH Auto move from waiting to actionable or to_review

--- ghtaskhub/sync.py
+++ ghtaskhub/sync.py
@@ -1,5 +1,7 @@
 import sys
 from collections import defaultdict
+import re
+import datetime
 
 from ghapi.all import GhApi
 
@@ -30,38 +32,44 @@ def sync(github_token, taskhub_repo, project_repo):
         return api.issues.get(number).state == "open"
 
     number_to_card_ids = defaultdict(list)
-    synced_something = False
-    for project in projects:
-        if not (project_repo == "all" or project_repo == project.name):
-            continue
-        synced_something = True
+
+    if project_repo == "all":
+        projects_to_sync = projects
+    else:
+        projects_to_sync = [
+            project for project in projects if project.name == project_repo
+        ]
+
+    if not projects_to_sync:
+        print(f"{project_repo} was not found in projects")
+        sys.exit(1)
+
+    for project in projects_to_sync:
         print(f"Syncing {project.name}")
         columns = taskhub_api.projects.list_columns(project.id)
-        done_id = None
-        other_ids = []
-        for column in columns:
-            if column.name == Column.DONE.value:
-                done_id = column.id
-            else:
-                other_ids.append(column.id)
 
-        if done_id is None:
-            print(f"There is not done column in {project.name}")
-            sys.exit(1)
+        column_name_to_enum = {v.value: v for k, v in Column.__members__.items()}
+        column_enum_to_ids = {
+            column_name_to_enum[column.name]: column.id for column in columns
+        }
+        other_ids = [
+            col_id for enum, col_id in column_enum_to_ids if enum != Column.DONE
+        ]
 
-        if not other_ids:
-            print(f"There are no other columns in {project.name}")
-            sys.exit(1)
-
+        # move cards to done if they were closed.
         for col_id in other_ids:
             cards = taskhub_api.projects.list_cards(col_id)
             for card in cards:
                 owner, repo, number, note_size = _get_info(card.note)
                 number_to_card_ids[number].append((note_size, card.id))
 
-                if _is_open(owner, repo, number):
-                    continue
-                _move_card(taskhub_api, card, done_id)
+                if not _is_open(owner, repo, number):
+                    _move_card(taskhub_api, card, column_enum_to_ids[Column.DONE])
+                elif column_enum_to_ids[Column.WAITING_FOR_RESPONSE] == col_id:
+                    repo_api = _get_repo_api(owner, repo)
+                    _move_waiting(
+                        taskhub_api, repo_api, card, column_enum_to_ids, number, owner
+                    )
 
     for number, id_infos in number_to_card_ids.items():
         if len(id_infos) <= 1:
@@ -72,6 +80,51 @@ def sync(github_token, taskhub_repo, project_repo):
         for _, card_id in sorted_info[:-1]:
             taskhub_api.projects.delete_card(card_id)
 
-    if not synced_something:
-        print(f"{project_repo} was not found in projects")
-        sys.exit(1)
+
+def _move_waiting(taskhub_api, repo_api, card, column_enum_to_ids, number, owner):
+    card_date_match = re.search("Added: (.+)", card.note)
+    if card_date_match is None:
+        return
+
+    card_date_str = card_date_match.group(1)
+    try:
+        card_added_dt = datetime.datetime.fromisoformat(card_date_str)
+    except ValueError:
+        return
+
+    info = repo_api.issues.get(number)
+    if not hasattr(info, "pul_request"):
+        return
+
+    pr = repo_api.pulls.get(number)
+
+    if pr.user == owner:
+        # MY PR & Review by someone else -> Move to "Actionable"
+        _ = repo_api.pulls.list_reviews(number, per_page=1)
+        last_page = repo_api.last_page()
+        last_review = repo_api.pulls.list_reviews(number, per_page=1, page=last_page)
+        if last_review.user.login == owner:
+            return
+
+        try:
+            review_dt = datetime.datetime.fromisoformat(
+                last_review.replace("Z", "+00:00")
+            )
+        except ValueError:
+            return
+
+        if review_dt > card_added_dt:
+            _move_card(taskhub_api, card, column_enum_to_ids[Column.ACTIONABLE])
+    else:
+        # Not my PR & updated by contributor -> Move to "To Review"
+        updated_at = pr.head.repo.updated_at
+
+        try:
+            updated_dt = datetime.datetime.fromisoformat(
+                updated_at.replace("Z", "+00:00")
+            )
+        except ValueError:
+            return
+
+        if updated_dt > card_added_dt:
+            _move_card(taskhub_api, card, column_enum_to_ids[Column.TO_REVIEW])
